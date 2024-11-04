727af543398bc681a90fae28cc8a038c3ae7452a
icorani <icorani@ya.ru>
2024-11-05 02:07:27 +0300

    fix: reformat code

--- src/app.ts
+++ src/app.ts
@@ -7,27 +7,27 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const opts: Partial<AutoloadPluginOptions> = {
-  ignoreFilter: (path: string) => {
-    const isFileNested = (path.match(new RegExp('/', 'g')) ?? []).length > 1;
-    if (!isFileNested) {
-      return false;
-    }
-    return !path.endsWith('index.js');
-  },
-  forceESM: true,
+    ignoreFilter: (path: string) => {
+        const isFileNested = (path.match(new RegExp('/', 'g')) ?? []).length > 1;
+        if (!isFileNested) {
+            return false;
+        }
+        return !path.endsWith('index.js');
+    },
+    forceESM: true,
 };
 
 const app: FastifyPluginAsync = async (fastify, _) => {
-  void fastify.register(AutoLoad, {
-    dir: join(__dirname, 'plugins'),
-    ...opts,
-  });
+    void fastify.register(AutoLoad, {
+        dir: join(__dirname, 'plugins'),
+        ...opts,
+    });
 
-  void fastify.register(AutoLoad, {
-    dir: join(__dirname, 'routes'),
-    routeParams: true,
-    ...opts,
-  });
+    void fastify.register(AutoLoad, {
+        dir: join(__dirname, 'routes'),
+        routeParams: true,
+        ...opts,
+    });
 };
 
 export default app;
